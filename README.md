8fed4181b529f9a69847eb1504ed0d3a257b6b6a
Ricardo Graça <ricardo@devius.net>
2013-01-09 13:13:33 +0000

    Fix install script and update some themes.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Themes for Gedit 3
+
+Version 0.1.0
+
+---
+
+This is based on the infamous mig/gedit-themes repository, but since it seems to be stagnant and not accepting pull
+requests anymore I decided it was time to make things happen again. This fixes the install script and updates some
+themes that looked terrible when the option to highlight the current line was selectd.
+
+The install script could use some more work, but should be fully functional as it is. Pull requests are welcomed.
+
+## Changelog
+
+### 0.1.0
+ - Replace install script with new one that works with Gedit 3
+ - Remove railscasts2 and Rainbow Neon themes
+ - Modify some themes that had no defined background color for current-line so that it always looks good
--- install.sh
+++ install.sh
@@ -1,4 +1,32 @@
 #!/bin/sh
-set -v
+#
+# Copyright (C) 2012 Ricardo Graça
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-sudo cp ./*.xml $HOME/.gnome2/gedit/styles/.
+# Notes:
+# This script only works for Gedit 3
+
+INSTALL_DIR="$HOME/.local/share/gedit/styles"
+
+echo "Installing Gedit themes..."
+
+if [ ! -d "$INSTALL_DIR" ]; then
+	mkdir "$INSTALL_DIR"
+fi
+
+cp ./*.xml $INSTALL_DIR/.
+
+echo "All done!"
